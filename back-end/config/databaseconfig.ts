4b1d928cd19ba092dd6ed03920a6eaa78a906e09
Micael Fone <micaelfone0@gmail.com>
2025-03-18 18:40:36 -0300

    feat: :sparkles: Adicionando o sequelize

---
+++ back-end/config/databaseconfig.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { Sequelize } from 'sequelize';
+ 
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+export default sequelize;
