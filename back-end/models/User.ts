c1848b41939ef050624720ce4b74c597dff21374
Micael Fone <micaelfone0@gmail.com>
2025-03-18 18:51:03 -0300

    fix: :bug: Corrigindo o tipo de cada coluna do model de usuário

---
+++ back-end/models/User.ts
@@ -0,0 +1,36 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/databaseconfig';
+
+const UserModel = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    resetPasswordToken: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    resetPasswordExpires: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: 'users',
+  }
+); 
+export default UserModel;
