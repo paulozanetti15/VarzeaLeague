e0ddbf2875aaa621ae2dbeb0c057cc881d63e6de
Micael Fone <micaelfone0@gmail.com>
2025-03-18 18:57:21 -0300

    chore: :fire: Mudando a estrutura do projeto

--- back-end/src/routes/passwordReset.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { requestPasswordReset, resetPassword } from '../controllers/passwordResetController';
-
-const router = Router();
-
-router.post('/request-reset', requestPasswordReset);
-router.post('/reset', resetPassword);
-
-export default router; 
