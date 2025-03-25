497636307cf3d4c2a71e261adcab2c08a211d7e6
Micael Fone <micaelfone0@gmail.com>
2025-03-24 23:25:47 -0300

    chore: :fire: Removendo app.ts

--- back-end/app.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import passwordRoutes from './routes/passwordRoutes';
-
-const app = express();
-
-// Registrar as rotas de recuperação de senha
-app.use('/api/password', passwordRoutes);
-
-// ... resto do código ... 
