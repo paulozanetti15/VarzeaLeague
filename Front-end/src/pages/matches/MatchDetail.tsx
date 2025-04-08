3f537785513c8aff5f8076c29006362c35f92b99
Micael Fone <micaelfone0@gmail.com>
2025-04-08 20:35:51 -0300

    feat:

---
+++ Front-end/src/pages/matches/MatchDetail.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function MatchDetail() {
+    return (
+        <div>
+        <h1>Match Detail</h1>
+        {/* Add your match detail content here */}
+        </div>
+    );
+}
+export default MatchDetail;
