7663382b2466f819327fb1da7ac80a067cfe7b51
Micael Fone <micaelfone0@gmail.com>
2025-04-27 10:44:14 -0300

    feat: :sparkles: Arrumando a parte de partidas

--- back-end/routes/matchRoutes.ts
+++ back-end/routes/matchRoutes.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { authenticateToken, AuthRequest } from '../middleware/auth';
-import { getMatchPlayers,joinMatch,leaveMatchPlayer,joinMatchByTeam } from '../controllers/MatchPlayerController';
+import { getMatchPlayers,leaveMatchPlayer,joinMatchByTeam } from '../controllers/MatchPlayerController';
 import {getMatch,listMatches,createMatch} from '../controllers/matchController';
 const router = express.Router();
 // Criar uma nova partida
@@ -18,14 +18,13 @@ router.get('/:id',getMatch,authenticateToken, async (req: Request, res: Response
 router.get('/:id/players',getMatchPlayers,authenticateToken, async (req: Request, res: Response): Promise<void> => {
 });
 
-router.post('/:id/join',joinMatch ,authenticateToken, async (req: AuthRequest, res: Response): Promise<void> => {
-});
-
 router.post('/:id/leave',leaveMatchPlayer ,authenticateToken, async (req: AuthRequest, res: Response): Promise<void> => {
 });
 
 // Entrar em uma partida com um time
 router.post('/:id/join-team',joinMatchByTeam,authenticateToken, async (req: AuthRequest, res: Response): Promise<void> => {
+});
+router.post('/:id/leave-team',leaveMatchPlayer ,authenticateToken, async (req: AuthRequest, res: Response): Promise<void> => {
 });
 
 export default router; 
