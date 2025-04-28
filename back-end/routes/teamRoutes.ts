9fd0fbdc0dde88c6848542dc588d041098030e63
Matheus Lucato <ytmatheuslucato@gmail.com>
2025-04-28 18:19:59 -0300

    correcao upload de imagem de time

--- back-end/routes/teamRoutes.ts
+++ back-end/routes/teamRoutes.ts
@@ -8,7 +8,6 @@ router.get('/', authenticateToken, TeamController.listTeams);
 router.get('/:id', authenticateToken, TeamController.getTeam);
 router.post('/', authenticateToken,upload.single('banner'), TeamController.create);
 router.put('/:id', authenticateToken, upload.single('banner'), TeamController.updateTeam);
-router.post('/:id/banner', authenticateToken, upload.single('banner'), TeamController.uploadBanner);
 router.delete('/:id', authenticateToken, TeamController.deleteTeam);
 
 export default router;
