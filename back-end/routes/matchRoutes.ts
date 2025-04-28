24c026814dd1cd5ff6069fc9d03d46e7f1de2ce6
Micael Fone <104154840+MIcaelFone@users.noreply.github.com>
2025-04-27 21:03:33 -0300

    Validando regras (#26)
    * feat: :sparkles: Adicionando para criar uploads
    
    * feat: :sparkles: Adicionando data nascimento e sexo para cadastrar usuário
    
    * feat: :sparkles: Adicionando categoria
    
    * feat: :see_no_evil: Ignorando os arquivos de upload
    
    * feat: :sparkles: Adicionando sexo e datanascimento
    
    * feat: :sparkles: Adicionando idademaxima, idademinima,maxparticipantes,sexo,primaryColor,secondaryColor,banner
    
    * style: :lipstick: Adicionando novas classes e arrumando o estilo de css
    
    * fix: :bug: Arrumando a forma como o arquivo do banner ou foto de perfil é salva
    
    * fix: :bug: Corrigindo a forma como está salvando a imagem
    
    * feat: :sparkles: Adicionando campo de categoria
    
    * feat: :sparkles: Adicionando campos de data nascimento e sexo
    
    * feat: :zap: Adicionando campo de categoria
    
    * feat: :sparkles: Adicionando campos de sexo e data nascimento
    
    * feat: :sparkles: Arrumando a parte de partidas
    
    * fix: :sparkles: Arrumando a parte do time entrar na partida

--- back-end/routes/matchRoutes.ts
+++ back-end/routes/matchRoutes.ts
@@ -1,30 +1,13 @@
 import express, { Request, Response } from 'express';
 import { authenticateToken, AuthRequest } from '../middleware/auth';
-import { getMatchPlayers,leaveMatchPlayer,joinMatchByTeam } from '../controllers/MatchPlayerController';
+import {joinMatchByTeam,getMattchTeams } from '../controllers/MatchPlayerController';
 import {getMatch,listMatches,createMatch} from '../controllers/matchController';
 const router = express.Router();
 // Criar uma nova partida
-router.post('/',createMatch,authenticateToken, async (req: AuthRequest, res: Response): Promise<void> => {
-  
-});
-
-router.get('/',listMatches,authenticateToken, async (req: Request, res: Response): Promise<void> => {
-});
-
-router.get('/:id',getMatch,authenticateToken, async (req: Request, res: Response): Promise<void> => {
-});
-
-// Endpoint detalhado para jogadores da partida específica
-router.get('/:id/players',getMatchPlayers,authenticateToken, async (req: Request, res: Response): Promise<void> => {
-});
-
-router.post('/:id/leave',leaveMatchPlayer ,authenticateToken, async (req: AuthRequest, res: Response): Promise<void> => {
-});
-
-// Entrar em uma partida com um time
-router.post('/:id/join-team',joinMatchByTeam,authenticateToken, async (req: AuthRequest, res: Response): Promise<void> => {
-});
-router.post('/:id/leave-team',leaveMatchPlayer ,authenticateToken, async (req: AuthRequest, res: Response): Promise<void> => {
-});
+router.post('/',createMatch,authenticateToken)
+router.get('/',listMatches,authenticateToken)
+router.get('/:id',getMatch,authenticateToken)
+router.post('/:id/join-team',joinMatchByTeam,authenticateToken)
+router.get('/:id/join-team',getMattchTeams,authenticateToken)
 
 export default router; 
