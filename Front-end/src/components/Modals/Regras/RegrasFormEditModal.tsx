215468ce5882dd0c223309522b514ce7a9b447c8
Micael Fone <micaelfone0@gmail.com>
2025-08-30 15:34:35 -0300

    Criando um modal para editar regras

---
+++ Front-end/src/components/Modals/Regras/RegrasFormEditModal.tsx
@@ -0,0 +1,322 @@
+import React, { useEffect, useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import axios from 'axios';
+import './RegrasStyles.css';
+import ToastComponent from '../../Toast/ToastComponent';
+import { format, parse, isValid, isAfter, set } from 'date-fns';
+
+
+
+interface RegrasFormEditModalProps {
+  show: boolean;
+  onHide: () => void;
+  onClose: () => void;
+  userId: number;
+  partidaDados: any;
+}
+interface RulesData {
+  dataLimite: string;
+  idadeMinima: number;
+  idadeMaxima: number;
+  genero: string;
+}
+
+const RegrasFormEditModal: React.FC<RegrasFormEditModalProps> = ({ show, onHide, userId, partidaDados,onClose }) => {
+  const [dataLimite, setDataLimite] = useState<string>(format(new Date(), 'dd/MM/yyyy'));
+  const [genero, setGenero] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [sucess, setSucess] = useState<string>("");
+  const [formData, setFormData] = useState<RulesData>({
+    dataLimite: format(new Date(), 'dd/MM/yyyy'),
+    idadeMinima: 0,
+    idadeMaxima: 0,
+    genero: ''
+  });
+  const [initialData, setInitialData] = useState<RulesData>({
+    dataLimite: format(new Date(), 'dd/MM/yyyy'),
+    idadeMinima: 0,
+    idadeMaxima: 0,
+    genero: ''
+  });
+  const [isChanged, setIsChanged] = useState(false);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === 'dataLimite') {
+      const dateRegex = /^(\d{0,2})\/(\d{0,2})\/(\d{0,4})$/;
+      
+      let formattedDate = value.replace(/\D/g, '');
+      
+      if (formattedDate.length <= 8) {
+        if (formattedDate.length > 4) {
+          formattedDate = formattedDate.replace(/(\d{2})(\d{2})(\d{0,4})/, '$1/$2/$3');
+        } else if (formattedDate.length > 2) {
+          formattedDate = formattedDate.replace(/(\d{2})(\d{0,2})/, '$1/$2');
+        }
+
+        if (dateRegex.test(formattedDate) || formattedDate.length < 10) {
+          setDataLimite(formattedDate);
+          setFormData(prev => ({
+            ...prev,
+            [name]: formattedDate
+          }));
+        }
+      }
+      return;
+    }
+    if (name === 'idadeMinima') {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: parseInt(value)
+      }));
+    }
+    if (name === 'idadeMaxima') {  
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: parseInt(value)
+      }));
+    }
+    if (name === 'genero') {
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]  : value
+        }));
+    }
+  };
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setGenero(value);
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const verificarDataLimite = (dataLimite: string, dataPartida: string): boolean => {
+    const parsedDataLimite = parse(dataLimite, 'dd/MM/yyyy', new Date());
+    const parsedDataPartida = new Date(dataPartida);
+
+    if (!isValid(parsedDataLimite)) {
+      setError('Data limite inválida. Use o formato DD/MM/AAAA');
+      return false;
+    }
+
+    if (!isAfter(parsedDataPartida, parsedDataLimite)) {
+      setError('A data limite deve ser anterior à data da partida');
+      return false;
+    }
+
+    return true;
+  };
+
+  const validarIdades = (): boolean => {
+    const minima = formData.idadeMinima;
+    const maxima = formData.idadeMaxima;
+
+    if (isNaN(minima) || minima < 0) {
+      setError('A idade mínima deve ser um número positivo');
+      return false;
+    }
+
+    if (isNaN(maxima) || maxima < 0) {
+      setError('A idade máxima deve ser um número positivo');
+      return false;
+    }
+
+    if (minima >maxima) {
+      setError('A idade mínima não pode ser maior que a idade máxima');
+      return false;
+    }
+
+    if (maxima > 100) {
+      setError('A idade máxima não pode ser maior que 100 anos');
+      return false;
+    }
+
+    return true;
+  };
+
+  const atualizaRegras = async (partidaDados: any) => {
+    setError('');
+    if (!dataLimite || !verificarDataLimite(dataLimite, partidaDados.date)) {
+      setError('Por favor, insira uma data limite válida no formato DD/MM/AAAA, anterior à data da partida.');
+      return;
+    }
+
+    if (formData.idadeMinima==0 || formData.idadeMaxima==0 || !validarIdades()) {
+       
+      setError('Por favor, insira idades válidas');
+      return;
+    }  
+
+    if (formData.genero=="") {
+      setError('Por favor, selecione o gênero da partida');
+      return;
+    }
+
+    try {
+        const response=await axios.put(
+          `http://localhost:3001/api/rules/${partidaDados.id}`,
+          {
+            userId: userId,
+            partidaId:partidaDados.id,
+            dataLimite: format(parse(dataLimite, 'dd/MM/yyyy', new Date()), 'yyyy-MM-dd'),
+            idadeMinima: formData.idadeMinima,
+            idadeMaxima: formData.idadeMaxima,
+            genero: formData.genero
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+          }
+        );
+
+        if (response.status === 200) {
+          setSucess('Regras atualizadas com sucesso!');
+          setTimeout(() => {
+           window.location.reload();
+          }, 500);
+          
+        }
+         
+      
+    } catch (error) {
+      setError('Erro ao atualizar regras. Tente novamente.');
+    }
+  };
+  const carregarRegras = async (partidaDados:any) => {
+    try {
+      const response = await axios.get(`http://localhost:3001/api/rules/${partidaDados.id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      });
+      if (response.status === 200) {
+        setFormData({
+          dataLimite:  format(new Date(response.data.dataLimite), 'dd/MM/yyyy'),
+          idadeMinima: response.data.idadeMinima.toString(),
+          idadeMaxima: response.data.idadeMaxima.toString(),
+          genero: response.data.genero
+        });
+        setInitialData({
+          dataLimite:  format(new Date(response.data.dataLimite), 'dd/MM/yyyy'),
+          idadeMinima: response.data.idadeMinima.toString(),
+          idadeMaxima: response.data.idadeMaxima.toString(),
+          genero: response.data.genero
+        });
+      }
+    } catch (error) {
+      setError(`Erro ao carregar regras. Tente novamente. ${error}`);
+    }
+  };
+  useEffect(() => {
+    carregarRegras(partidaDados);
+  }, [partidaDados]);
+
+  useEffect(() => {
+    JSON.stringify(formData) !== JSON.stringify(initialData) ? setIsChanged(true) : setIsChanged(false);
+  }, [formData, initialData]);
+
+  return (
+    <>
+      
+      <Modal
+        show={show}
+        onHide={onHide}
+        className="regras-modal"
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Body>
+          <div className="modal-content-wrapper">
+            <h2 className="modal-title">Regras da Partida</h2>
+            {error && (
+              <div className="error-message">
+                <p>{error}</p>
+              </div>
+            )}
+            {sucess && (
+              <div className="success-message">
+                <p style={{ justifyContent: 'center', display: 'flex' }}>{sucess}</p>
+              </div>
+            )}
+            <br/>
+            <div className="form-group">
+              <label>Data Limite para Inscrição</label>
+               <input
+                type="text"
+                id="dataLimite"
+                name="dataLimite"
+                value={formData.dataLimite}
+                onChange={handleInputChange}
+                required
+                className="form-control"
+                placeholder="DD/MM/AAAA"
+                maxLength={10}
+              />
+            </div>
+            <div className="form-row">
+              <div className="form-group">
+                <label>Idade Mínima</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  value={formData.idadeMinima}
+                  onChange={handleInputChange}
+                  min="0"
+                  max="100"
+                  required
+                  placeholder="Ex: 18"
+                  name="idadeMinima"
+                />
+              </div>
+              <div className="form-group">
+                <label>Idade Máxima</label>
+                <input
+                  name='idadeMaxima'
+                  type="number"
+                  className="form-control"
+                  value={formData.idadeMaxima}
+                  onChange={handleInputChange}
+                  min="0"
+                  max="100"
+                  required
+                  placeholder="Ex: 35"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label>Gênero</label>
+              <select
+                name='genero'
+                className="form-control"
+                value={formData.genero}
+                onChange={handleSelectChange}
+                required
+              >
+                <option value="">Selecione o gênero</option>
+                <option value="Masculino">Masculino</option>
+                <option value="Feminino">Feminino</option>
+                <option value="Ambos">Ambos</option>
+              </select>
+            </div>
+            <div className="modal-buttons">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={()=>atualizaRegras(partidaDados)}
+                disabled={isChanged ? false : true}
+              >
+                Atualizar Regras
+              </button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+};
+
+
+export default RegrasFormEditModal;
