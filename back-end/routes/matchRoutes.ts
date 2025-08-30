58d0b797b58ffd3ce85c1b2d3a4c0da4542f1c8d
Micael Fone <micaelfone0@gmail.com>
2025-08-30 15:33:52 -0300

    Adicionando uma nova rota para atualizar

--- back-end/routes/matchRoutes.ts
+++ back-end/routes/matchRoutes.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { authenticateToken, AuthRequest } from '../middleware/auth';
 import {joinMatchByTeam,getMatchTeams, deleteTeamMatch,getTeamsAvailable, checkTeamsRuleCompliance} from '../controllers/MatchTeamsController';
-import { createMatch, listMatches, getMatch, deleteMatch } from '../controllers/matchController';
+import { createMatch, listMatches, getMatch, deleteMatch, updateMatch } from '../controllers/matchController';
 
 const router = express.Router();
 
@@ -15,5 +15,6 @@ router.post('/:id/join-team', authenticateToken, joinMatchByTeam);
 router.get('/:id/join-team', authenticateToken, getMatchTeams);
 router.delete('/:id/join-team/:teamId', authenticateToken, deleteTeamMatch);
 router.get('/:id/check-teams-rule-compliance', authenticateToken, checkTeamsRuleCompliance);
+router.put('/:id', authenticateToken, updateMatch);
 
 export default router; 
