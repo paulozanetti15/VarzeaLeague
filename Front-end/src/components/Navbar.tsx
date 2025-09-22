f891e709f1afc45353f26e9cffefb5c04190bbdd
Micael Fone <micaelfone0@gmail.com>
2025-09-22 19:15:38 -0300

    Adicionando historico como aba

--- Front-end/src/components/Navbar.tsx
+++ Front-end/src/components/Navbar.tsx
@@ -17,6 +17,7 @@ import {
   Badge,
   Chip,
 } from '@mui/material';
+
 import {
   Menu as MenuIcon,
   SportsSoccer,
@@ -28,12 +29,17 @@ import {
   Notifications,
   ArrowBack,
   CalendarMonth,
+  HistoryIcon,
   Search,
 } from '@mui/icons-material';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { getRoleName } from '../utils/roleUtils';
 import '../components/landing/Header.css';
+import {
+   History
+} from '@mui/icons-material';
+
 
 // Pages are computed per role (see visiblePages below)
 
@@ -98,6 +104,7 @@ const Navbar = () => {
         return [
           { name: 'Dashboard', path: '/dashboard', icon: <Dashboard /> },
           { name: 'Times', path: '/teams', icon: <People /> },
+          { name: 'Histórico', path: '/historico', icon: <History/> },
         ];
       case 4: // Usuário Comum
         return [
